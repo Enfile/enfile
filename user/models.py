fba1d08439a48aa66635f219d40c2b07d26c3fe6
kosa3120 <kosa.kaito72@gmail.com>
2019-12-03 16:55:46 +0900

    userアプリケーションのmodel作成

--- user/models.py
+++ user/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import uuid as uuid_lib
 
-# Create your models here.
+class Profile(models.Model):
+    profile_id = models.UUIDField(
+        db_index=True,
+        default=uuid_lib.uuid4,
+        editable=False
+    )
+
+    icon_path = models.CharField(max_length=256, blank=True, null=True) 
+    name = models.CharField(max_length=256, blank=True, null=True)
+    sex = models.IntegerField(default=0)
+    birth_day = models.DateField(blank=True, null=True)
+    school_year = models.IntegerField(blank=True, null=True)
+    school_name = models.CharField(max_length=256, blank=True, null=True)
+    profile = models.CharField(max_length=256, blank=True, null=True)
+    using_os = models.CharField(max_length=256, blank=True, null=True) 
+    link = models.CharField(max_length=256, blank=True, null=True)
+    contact = models.CharField(max_length=256, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class User(models.Model):
+    user_id = models.UUIDField(
+      db_index=True,
+      default=uuid_lib.uuid4,
+      editable=False
+    )
+
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
