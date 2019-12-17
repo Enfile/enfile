d05fb3b5800639daed568671c54bcae6823ce579
kosa3120 <kosa.kaito72@gmail.com>
2019-12-17 16:52:01 +0900

    userを作成、編集するときにprofileも変更がかかるようにした

--- user/serializers.py
+++ user/serializers.py
@@ -3,14 +3,38 @@ from rest_framework import serializers
 from .models import Profile, User
 
 class ProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Profile
-        fields = ('profile_id','icon_path','name','sex','birth_day','school_year','school_name','profile','using_os','link','contact','created_at','updated_at')
+        fields = (
+            'profile_id',
+            'icon_path',
+            'name',
+            'sex',
+            'birth_day',
+            'school_year',
+            'school_name',
+            'profile',
+            'using_os',
+            'link',
+            'contact',
+            'created_at',
+            'updated_at'
+            )
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     profile = ProfileSerializer(required=False)
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        profile = Profile.objects.create(**profile_data)
+        user = User.objects.create(profile=profile, **validated_data)
+        return user
+        
     class Meta:
         model = User
         fields = ('user_id','profile','is_active','created_at','updated_at')
+
+     
